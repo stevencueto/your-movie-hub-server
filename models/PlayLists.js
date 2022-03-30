7c547b65353e82c9873e848ef95d85992a214600
stevencueto <steven.cueto1305@gmail.com>
2022-03-30 16:29:23 -0400

    fixing playlist movie bug

--- models/PlayLists.js
+++ models/PlayLists.js
@@ -2,17 +2,30 @@ const { text } = require('express');
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
+const movieSchema = new Schema({
+    name: {
+      type: String,
+    },
+    title: {
+        type: String,
+      },
+    poster_path: {
+      type: String,
+    },
+    overview: {
+        type: String,
+    },
+    id: {
+      type: Number,
+      unique: true
+    },
+  });
 
 const payListSchema = new Schema({
     name: {
         type: String
     },
-    movie:[
-            {
-                type: Object,
-                unique: false
-            }
-    ],
+    movie: [[movieSchema]],
     user: {
         type: Schema.Types.ObjectId,
         ref: "User",
@@ -22,6 +35,7 @@ const payListSchema = new Schema({
         type: String
     }
 })
+
 
 const PlayList = mongoose.model('PlayList', payListSchema)
 module.exports = PlayList;
